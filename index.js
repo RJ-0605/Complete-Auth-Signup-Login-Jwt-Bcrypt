41c64263da40e9270ad5cd5b6ca65c6c6d7c3776
rj-0605 <rojedkay@gmail.com>
2020-12-03 16:47:24 +0000

    authentication and signup and login wich generates jwt if the bcrypt of passwords match  with trail test on a path to verify the jwt webtoken passed to it

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+// Mongodb Connection
+require('./config/database');
+
+const loginRouter = require('./controllers/login')
+const SignupRouter = require('./controllers/signup')
+const HomeRouter = require('./controllers/home')
+
+const app = express()
+const port = 3000
+
+// parse application/x-www-form-urlencoded for picking data or params
+// in post request of forms 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+app.use('/signup', SignupRouter)
+
+
+app.use('/login', loginRouter)
+
+app.use('/', HomeRouter)
+
+
+
+
+
+app.listen(port, () => console.log(`Started App on port : ${port}`))
+
+
+
+
+
